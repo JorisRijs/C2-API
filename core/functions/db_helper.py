3041aba983d950627ed2d33514741603cb84c187
Joris Rijs <joris@freya22.nl>
2023-09-10 15:53:23 +0200

    Added a database helper file

---
+++ core/functions/db_helper.py
@@ -0,0 +1,31 @@
+
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm.attributes import set_attribute
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+from models.agent import Agent
+from models.key import Key
+from models.listener import Listener
+from models.result import Result
+from models.task import Task
+
+if not os.path.exists('../../DB'):
+    os.mkdir('../../DB')
+    engine = sqlalchemy.create_engine('sqllite///../../DB/app.db', echo=False, convert_unicode=True)
+else:
+    engine = sqlalchemy.create_engine('sqllite///app.db', echo=False, convert_unicode=True)
+base = declarative_base()
+
+db_session = scoped_session(sessionmaker(autocommit = False, autoflush=False, bind = engine))
+
+def init_db():
+    base.metadata.create_all(bind=engine)
+
+def set_property(object_type, object_ID, attribute, value):
+    try:
+        obj = db_session.query(object_type).filter(object_type.id == object_ID).first()
+        setattr(obj, attribute, value)
+    except sqlalchemy.orm.exc.NoResultFound:
+        return None
